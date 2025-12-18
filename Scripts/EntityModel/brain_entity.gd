dc6c10fb109e2111b73797b2926840aa4b295d7b
Chase Odom <chasemichaelodom@gmail.com>
2025-12-18 12:31:04 -0700

    🚚(Entity Model) All entities are Area2Ds

--- Scripts/EntityModel/brain_entity.gd
+++ Scripts/EntityModel/brain_entity.gd
@@ -1,5 +1,11 @@
 @abstract
 class_name BrainEntity extends Entity
 
-@export var max_health : float
-@export var health : float
+@export var max_health : float = 100.0
+@export var health : float = 100.0
+
+## Area of this BrainEntity's attack.
+@export var hitBox : Area2D
+
+## Area of which points count for other attacks hitting this BrainEntity.
+@export var hurtBox : Area2D
--- Scripts/EntityModel/entity.gd
+++ Scripts/EntityModel/entity.gd
@@ -1,4 +1,4 @@
 @abstract
-class_name Entity extends Node2D
+class_name Entity extends Area2D
 
 signal world_item_dropped(sender : Node2D, item : Item, cooldown: float)
