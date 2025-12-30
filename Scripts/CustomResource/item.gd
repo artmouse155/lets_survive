3229667114818ab443b149e94c7f3d3863c50e77
Chase Odom <chasemichaelodom@gmail.com>
2025-12-30 14:07:25 -0800

    ✨(item structure) Adding StringName and tool group

--- Scripts/CustomResource/item.gd
+++ Scripts/CustomResource/item.gd
@@ -2,15 +2,15 @@ class_name Item extends Resource
 
 #region item_names
 
-const COOL_SWORD := "cool_sword"
-const STONE := "stone"
-const WOOD := "wood"
+const COOL_SWORD : StringName = "cool_sword"
+const STONE : StringName = "stone"
+const WOOD : StringName = "wood"
 
 #endregion
 
 #region item_data
 const _DEFAULT_ITEM_DATA : ItemData = preload("uid://4rv5ykr3xj2q")
-const _ITEM_DATA : Dictionary[String,ItemData] = {
+const _ITEM_DATA : Dictionary[StringName,ItemData] = {
 	COOL_SWORD : preload("uid://hbvcijgt3nv6"),
 	STONE : preload("uid://csa57185p0hmc"),
 	WOOD : preload("uid://bwekp0hhl047")
@@ -19,7 +19,7 @@ const _ITEM_DATA : Dictionary[String,ItemData] = {
 
 enum TOOL_TYPE {NONE, FISTS, SWORD}
 
-@export var item_name : String = WOOD
+@export var item_name : StringName = WOOD
 @export var item_quantity : int = 1
 
 func is_full() -> bool:
@@ -33,21 +33,21 @@ func is_tool() -> bool:
 	var tool_type : TOOL_TYPE = static_tool_type(item_name)
 	return (tool_type != TOOL_TYPE.NONE)
 
-static func _get_item_data(item : String) -> ItemData:
+static func _get_item_data(item : StringName) -> ItemData:
 	if item in _ITEM_DATA.keys():
 		return _ITEM_DATA[item]
 	return _DEFAULT_ITEM_DATA
 
-static func static_max(item : String) -> int:
+static func static_max(item : StringName) -> int:
 	var item_data : ItemData = _get_item_data(item)
 	return item_data.get_max_amount()
 
-static func static_tool_type(item : String) -> TOOL_TYPE:
+static func static_tool_type(item : StringName) -> TOOL_TYPE:
 	var item_data : ItemData = _get_item_data(item)
 	assert(item_data is ItemData)
 	return item_data.get_tool_type()
 
-static func static_texture(item: String) -> Texture2D:
+static func static_texture(item: StringName) -> Texture2D:
 	var item_data : ItemData = _get_item_data(item)
 	return item_data.get_icon()
 
@@ -66,7 +66,7 @@ func add(quantity : int) -> void:
 func subtract(quantity : int) -> void:
 	item_quantity -= quantity
 
-func _init(_item_name : String = WOOD, _item_quantity : int = 1) -> void:
+func _init(_item_name : StringName = WOOD, _item_quantity : int = 1) -> void:
 	self.item_name = _item_name
 	self.item_quantity = _item_quantity
 
--- Scripts/CustomResource/item_data.gd
+++ Scripts/CustomResource/item_data.gd
@@ -2,6 +2,8 @@ class_name ItemData extends Resource
 
 @export var _icon : Texture2D = null
 @export var _max_amount : int = 10
+@export_group("Tool","_tool")
+@export_custom(PROPERTY_HINT_GROUP_ENABLE, "") var _tool_enabled : bool = false
 @export var _tool_type : Item.TOOL_TYPE = Item.TOOL_TYPE.NONE
 
 func get_icon() -> Texture2D:
@@ -11,4 +13,4 @@ func get_max_amount() -> int:
 	return _max_amount
 
 func get_tool_type() -> Item.TOOL_TYPE:
-	return _tool_type
+	return _tool_type if _tool_enabled else Item.TOOL_TYPE.NONE
