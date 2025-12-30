3f5b9b8173b078dbd003f4db0edb14b161259d80
Chase Odom <chasemichaelodom@gmail.com>
2025-12-30 13:58:04 -0800

    🚧(item structure) Using ItemData
    Tried to define the giant item_data dict in item_data.gd, but that caused errors.

--- Scripts/CustomResource/item.gd
+++ Scripts/CustomResource/item.gd
@@ -1,14 +1,25 @@
 class_name Item extends Resource
 
 #region item_names
-const WOOD := "wood"
-const STONE := "stone"
+
 const COOL_SWORD := "cool_sword"
+const STONE := "stone"
+const WOOD := "wood"
+
+#endregion
+
+#region item_data
+const _DEFAULT_ITEM_DATA : ItemData = preload("uid://4rv5ykr3xj2q")
+const _ITEM_DATA : Dictionary[String,ItemData] = {
+	COOL_SWORD : preload("uid://hbvcijgt3nv6"),
+	STONE : preload("uid://csa57185p0hmc"),
+	WOOD : preload("uid://bwekp0hhl047")
+}
 #endregion
 
 enum TOOL_TYPE {NONE, FISTS, SWORD}
 
-@export var item_name : String = "wood"
+@export var item_name : String = WOOD
 @export var item_quantity : int = 1
 
 func is_full() -> bool:
@@ -23,7 +34,9 @@ func is_tool() -> bool:
 	return (tool_type != TOOL_TYPE.NONE)
 
 static func _get_item_data(item : String) -> ItemData:
-	return ItemData.of(item)
+	if item in _ITEM_DATA.keys():
+		return _ITEM_DATA[item]
+	return _DEFAULT_ITEM_DATA
 
 static func static_max(item : String) -> int:
 	var item_data : ItemData = _get_item_data(item)
@@ -31,6 +44,7 @@ static func static_max(item : String) -> int:
 
 static func static_tool_type(item : String) -> TOOL_TYPE:
 	var item_data : ItemData = _get_item_data(item)
+	assert(item_data is ItemData)
 	return item_data.get_tool_type()
 
 static func static_texture(item: String) -> Texture2D:
@@ -52,7 +66,7 @@ func add(quantity : int) -> void:
 func subtract(quantity : int) -> void:
 	item_quantity -= quantity
 
-func _init(_item_name : String = "wood", _item_quantity : int = 1) -> void:
+func _init(_item_name : String = WOOD, _item_quantity : int = 1) -> void:
 	self.item_name = _item_name
 	self.item_quantity = _item_quantity
 
--- Scripts/CustomResource/item_data.gd
+++ Scripts/CustomResource/item_data.gd
@@ -1,17 +1,8 @@
 class_name ItemData extends Resource
 
-const _DEFAULT_ITEM_DATA : ItemData = preload("uid://4rv5ykr3xj2q")
-
-#region item_data
-const _DATA : Dictionary[String,ItemData] = {
-	Item.WOOD : preload("uid://bwekp0hhl047"),
-	Item.STONE : preload("uid://csa57185p0hmc"),
-	Item.COOL_SWORD : preload("uid://hbvcijgt3nv6")
-}
-#endregion
-
 @export var _icon : Texture2D = null
 @export var _max_amount : int = 10
+@export var _tool_type : Item.TOOL_TYPE = Item.TOOL_TYPE.NONE
 
 func get_icon() -> Texture2D:
 	return _icon
@@ -20,9 +11,4 @@ func get_max_amount() -> int:
 	return _max_amount
 
 func get_tool_type() -> Item.TOOL_TYPE:
-	return Item.TOOL_TYPE.NONE
-
-static func of(item : String) -> ItemData:
-	if item in _DATA.keys():
-		return _DATA[item]
-	return _DEFAULT_ITEM_DATA
+	return _tool_type
